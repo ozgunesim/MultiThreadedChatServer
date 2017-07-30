0ac978145c792f7bfe521c85183960cea58f1f4a
ozgunesim <ozgunesim@gmail.com>
2017-07-30 15:08:14 +0300

    first commit

---
+++ MultiThreadChatServer/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace MultiThreadChatServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(615, 274);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Gönderen";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Mesaj";
+            this.columnHeader2.Width = 337;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Zaman";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 274);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+
+    }
+}
+
---
+++ MultiThreadChatServer/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace MultiThreadChatServer
+{
+    public partial class Form1 : Form
+    {
+        Thread th;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            th = new Thread(new ThreadStart(dinle));
+            try
+            {
+                th.Start();
+                th.Priority = ThreadPriority.Highest;
+                this.Text = "Server açıldı";
+            }
+            catch (ThreadAbortException hata)
+            {
+                th.Abort();
+                MessageBox.Show("Bekleyen paket yok");
+            } 
+        }
+
+        const int port = 10000;
+        private void dinle()
+        {
+            
+            string ip;
+            string gelen;
+            TcpListener dinleyici = new TcpListener(port);
+            byte[] gelenveri_dizi = new byte[1024];
+            
+            while (true)
+            {
+                dinleyici.Start();
+                Socket soket = dinleyici.AcceptSocket();
+                soket.Receive(gelenveri_dizi, gelenveri_dizi.Length, 0);
+                gelen = Encoding.ASCII.GetString(gelenveri_dizi);
+                ip = soket.RemoteEndPoint.ToString();
+                ListViewItem itm = new ListViewItem();
+                itm.Text = ip;
+                itm.SubItems.Add(gelen);
+                itm.SubItems.Add(DateTime.Now.ToString());
+                listView1.Items.Add(itm);
+
+                string cevap = "Mesajiniz basariyla alinmistir." + Environment.NewLine +
+                    "Gelen: "  + gelen + Environment.NewLine;
+                byte[] gonderveri_dizi = Encoding.ASCII.GetBytes(cevap);
+                soket.Send(gonderveri_dizi, gonderveri_dizi.Length, 0);
+
+                gelen = "";
+                dinleyici.Stop();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //TcpListener dinleyici = new TcpListener(port);
+            //dinleyici.Start();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            th.Abort();   
+        }
+
+
+    }
+}
